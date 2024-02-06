eb2af7595688a5f7d306798a4b706b0a3011638f
uiw6unoh <uiw6unoh@naver.com>
2024-02-06 10:54:13 +0900

    Update : Animated Cursor - Sparkle

--- src/components/Sparkle.tsx
+++ src/components/Sparkle.tsx
@@ -1,134 +1,158 @@
-import React, { useEffect, useRef } from "react"
+import React, { useState, useEffect } from "react"
 
-const Sparkle = () => {
+interface StarOrDot {
+  left: number
+  top: number
+  color: string
+  visible: boolean
+  size: number
+  life?: number
+}
+
+function throttle<F extends (...args: any[]) => any>(
+  func: F,
+  limit: number
+): (...args: Parameters<F>) => void {
+  let lastFunc: number
+  let lastRan: number
+
+  return function (this: ThisParameterType<F>, ...args: Parameters<F>) {
+    const context = this
+    if (!lastRan) {
+      func.apply(context, args)
+      lastRan = Date.now()
+    } else {
+      clearTimeout(lastFunc)
+      lastFunc = window.setTimeout(() => {
+        if (Date.now() - lastRan >= limit) {
+          func.apply(context, args)
+          lastRan = Date.now()
+        }
+      }, limit - (Date.now() - lastRan))
+    }
+  }
+}
+
+const Sparkle = (): JSX.Element => {
   const sparkles = 50
-  const starsRef = useRef<Array<HTMLDivElement | null>>(
-    Array(sparkles).fill(null)
-  )
-  const tinyRef = useRef<Array<HTMLDivElement | null>>(
-    Array(sparkles).fill(null)
-  )
-
-  const animationFrameRef = useRef<number | null>(null)
-
-  const newColour = () => {
-    const c = [
-      255,
-      Math.floor(Math.random() * 256),
-      Math.floor(Math.random() * 256),
-    ].sort(() => 0.5 - Math.random())
-    return `rgb(${c[0]}, ${c[1]}, ${c[2]})`
-  }
-
-  const createStarElements = () => {
-    for (let i = 0; i < sparkles; i++) {
-      const star = document.createElement("div")
-      const tiny = document.createElement("div")
-
-      star.style.cssText =
-        "position:absolute;width:5px;height:5px;background-color:transparent;visibility:hidden;z-index:9999;"
-      tiny.style.cssText =
-        "position:absolute;width:2px;height:2px;background-color:transparent;visibility:hidden;z-index:9999;"
-
-      document.body.appendChild(star)
-      document.body.appendChild(tiny)
-
-      starsRef.current[i] = star
-      tinyRef.current[i] = tiny
-    }
-  }
-
-  const updateStarsAndTiny = () => {
-    starsRef.current.forEach((star, i) => {
-      if (star && star.style.visibility === "visible") {
-        const starY = parseInt(star.style.top, 10)
-        const starX = parseInt(star.style.left, 10)
-
-        if (starY < window.innerHeight + window.scrollY) {
-          star.style.top = `${starY + 1 + Math.random() * 3}px`
-          star.style.left = `${starX + ((i % 5) - 2) / 5}px`
-        } else {
-          star.style.visibility = "hidden"
-        }
-      }
-    })
-
-    tinyRef.current.forEach((tiny, i) => {
-      if (tiny && tiny.style.visibility === "visible") {
-        const tinyY = parseInt(tiny.style.top, 10)
-        const tinyX = parseInt(tiny.style.left, 10)
-
-        if (tinyY < window.innerHeight + window.scrollY) {
-          tiny.style.top = `${tinyY + 1 + Math.random() * 3}px`
-          tiny.style.left = `${tinyX + ((i % 5) - 2) / 5}px`
-        } else {
-          tiny.style.visibility = "hidden"
-        }
-      }
-    })
-
-    animationFrameRef.current = requestAnimationFrame(updateStarsAndTiny)
-  }
-
-  const throttle = (
-    func: (this: any, ...args: any[]) => void,
-    limit: number
-  ) => {
-    let lastFunc: number | NodeJS.Timeout | undefined
-    let lastRan: number
-    return function (this: any, ...args: any[]) {
-      const context = this
-      if (!lastRan) {
-        func.apply(context, args)
-        lastRan = Date.now()
-      } else {
-        clearTimeout(lastFunc as number)
-        lastFunc = setTimeout(() => {
-          if (Date.now() - lastRan >= limit) {
-            func.apply(context, args)
-            lastRan = Date.now()
-          }
-        }, limit - (Date.now() - lastRan))
-      }
-    }
-  }
-
-  const handleMouseMove = throttle((e: { pageX: any; pageY: any }) => {
-    const mouseX = e.pageX
-    const mouseY = e.pageY
-
-    starsRef.current.some((star, i) => {
-      if (star && star.style.visibility === "hidden") {
-        star.style.left = `${mouseX}px`
-        star.style.top = `${mouseY}px`
-        star.style.backgroundColor = newColour()
-        star.style.clipPath = "rect(0px, 5px, 5px, 0px)"
-        star.style.visibility = "visible"
-        return true // Break the loop
-      }
-      return false // Continue the loop
-    })
-  }, 100) // 100ms 간격으로 함수 실행 제한
+  const [stars, setStars] = useState<StarOrDot[]>([])
+  const [tinyDots, setTinyDots] = useState<StarOrDot[]>([])
 
   useEffect(() => {
-    createStarElements()
-    updateStarsAndTiny()
-
-    document.addEventListener("mousemove", handleMouseMove)
-
-    return () => {
-      if (animationFrameRef.current !== null) {
-        cancelAnimationFrame(animationFrameRef.current)
+    // 마우스 움직임 핸들러 : 화면에 최대 별 개수에 도달하지 않았을 때 새 별을 추가
+    const moveHandler = throttle((e: MouseEvent) => {
+      if (stars.length < sparkles) {
+        setStars((prevStars) => [
+          ...prevStars,
+          {
+            left: e.pageX,
+            top: e.pageY,
+            color: newColour(),
+            visible: true,
+            size: 7,
+          },
+        ])
       }
-      document.removeEventListener("mousemove", handleMouseMove)
-      starsRef.current.forEach(
-        (star) => star && document.body.removeChild(star)
-      )
-      tinyRef.current.forEach((tiny) => tiny && document.body.removeChild(tiny))
-    }
-  }, [])
+    }, 300)
 
-  return null
+    document.addEventListener("mousemove", moveHandler)
+    return () => document.removeEventListener("mousemove", moveHandler)
+  }, [stars.length])
+
+  useEffect(() => {
+    const update = () => {
+      const [updatedStars, updatedTinyDots] = updateStarsAndDots(
+        stars,
+        tinyDots
+      )
+      setStars(updatedStars)
+      setTinyDots(updatedTinyDots)
+      requestAnimationFrame(update)
+    }
+    requestAnimationFrame(update)
+  }, [stars, tinyDots]) // 별과 작은 점 상태 업데이트에 따라 애니메이션 갱신
+
+  return (
+    <>
+      {stars.map((star, i) => (
+        <Star key={i} {...star} />
+      ))}
+      {tinyDots.map((dot, i) => (
+        <TinyDot key={i} {...dot} />
+      ))}
+    </>
+  )
+}
+
+// 초기 별 생성 및 상태 설정
+const Star = ({ left, top, color, visible, size }: StarOrDot) => (
+  <div
+    style={{
+      position: "absolute",
+      left: `${left}px`,
+      top: `${top}px`,
+      width: `${size}px`,
+      height: `${size}px`,
+      backgroundColor: color,
+      visibility: visible ? "visible" : "hidden",
+      zIndex: 9999,
+      // 별 모양을 위한 스타일
+      clipPath:
+        // "polygon(50% 0%, 50% 40%, 100% 40%, 100% 60%, 50% 60%, 50% 100%, 40% 100%, 40% 60%, 0% 60%, 0% 40%, 40% 40%, 40% 0%)"
+        "polygon(40% 0%, 60% 0%, 60% 40%, 100% 40%, 100% 60%, 60% 60%, 60% 100%, 40% 100%, 40% 60%, 0% 60%, 0% 40%, 40% 40%)",
+    }}
+  />
+)
+
+// 작은 점 생성 및 상태 설정
+const TinyDot = ({ left, top, color, visible }: StarOrDot) => (
+  <div
+    style={{
+      position: "absolute",
+      left: `${left}px`,
+      top: `${top}px`,
+      width: "2px",
+      height: "2px",
+      backgroundColor: color,
+      visibility: visible ? "visible" : "hidden",
+      zIndex: 9999,
+    }}
+  />
+)
+
+const updateStarsAndDots = (
+  stars: StarOrDot[],
+  tinyDots: StarOrDot[]
+): [StarOrDot[], StarOrDot[]] => {
+  // 별과 작은 점의 위치 및 가시성 업데이트
+  const updatedStars = stars
+    .map((star) => ({
+      ...star,
+      top: star.top + 1 + Math.random() * 3,
+      left: star.left + (Math.random() - 0.5) * 2,
+      visible: star.size > 2,
+      size: star.size > 2 ? star.size - 0.05 : 2, // 별의 크기 감소 속도 조절
+      life: star.size > 2 ? star.life : (star.life || 0) + 0.5, // 별의 수명 증가량 조절
+    }))
+    .filter((star) => star.size > 2 || (star.life && star.life < 100)) // life 조건도 조절해야 할 수 있음
+
+  const updatedTinyDots = tinyDots
+    .map((dot) => ({
+      ...dot,
+      life: (dot.life || 0) + 0.5, // 점의 수명 증가량 조절
+    }))
+    .filter((dot) => dot.life && dot.life < 100) // 점이 사라지는 조건 조절
+
+  return [updatedStars, updatedTinyDots]
+}
+
+const newColour = (): string => {
+  const c = [
+    255,
+    Math.floor(Math.random() * 256),
+    Math.floor(Math.random() * 256),
+  ].sort(() => 0.5 - Math.random())
+  return `rgb(${c[0]}, ${c[1]}, ${c[2]})`
 }
 
 export default Sparkle
