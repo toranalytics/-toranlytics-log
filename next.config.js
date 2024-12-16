96218842ddcf8f5d626aa0e6ae0f7c01be7e4aa8
toranalytics <tjwnsqja0704@gmail.com>
2024-12-16 11:15:30 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
-module.exports = {
+const { withSitemap } = require('next-sitemap');
+
+module.exports = withSitemap({
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
   },
-}
+  sitemap: {
+    siteUrl: 'https://toranalytics.vercel.app/', // 자신의 사이트 URL로 변경하세요.
+    generateRobotsTxt: true, // robots.txt 파일 생성
+  },
+});
