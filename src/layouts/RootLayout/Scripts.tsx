91a2179e54de04fb2e9e82188a9af8b7a89b7013
toranalytics <tjwnsqja0704@gmail.com>
2024-12-16 13:42:07 +0900

    Update Scripts.tsx

--- src/layouts/RootLayout/Scripts.tsx
+++ src/layouts/RootLayout/Scripts.tsx
@@ -6,14 +6,14 @@ const Scripts: React.FC = () => (
     {CONFIG?.googleAnalytics?.enable === true && (
       <>
         <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=GTM-5XX526WH`}
-          <!-- ${CONFIG.googleAnalytics.config.measurementId} -->
+          src={`https://www.googletagmanager.com/gtag/js?id=${CONFIG.googleAnalytics.config.measurementId}`}
+          
         />
         <Script strategy="lazyOnload" id="ga">
           {`window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'GTM-5XX526WH', {
+            gtag('config', '${CONFIG.googleAnalytics.config.measurementId}', {
               page_path: window.location.pathname,
             });`}
         </Script>
