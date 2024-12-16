9b1ab8e6b436178762e8e5fa33fe31e64e6de247
toranalytics <tjwnsqja0704@gmail.com>
2024-12-16 14:10:51 +0900

    Update TagList.tsx

--- src/routes/Feed/TagList.tsx
+++ src/routes/Feed/TagList.tsx
@@ -1,46 +1,32 @@
+// TagList.tsx
+import React from "react"
 import styled from "@emotion/styled"
 import { useRouter } from "next/router"
-import React from "react"
-import { Emoji } from "src/components/Emoji"
-import { useTagsQuery } from "src/hooks/useTagsQuery"
 
-type Props = {}
+type Props = {
+  data: Record<string, number>
+}
 
-const TagList: React.FC<Props> = () => {
+const TagList: React.FC<Props> = ({ data }) => {
   const router = useRouter()
-  const currentTag = router.query.tag || undefined
-  const data = useTagsQuery()
+  const currentTag = `${router.query.tag || ``}` || undefined
 
-  const handleClickTag = (value: any) => {
-    // delete
-    if (currentTag === value) {
-      router.push({
-        query: {
-          ...router.query,
-          tag: undefined,
-        },
-      })
-    }
-    // add
-    else {
-      router.push({
-        query: {
-          ...router.query,
-          tag: value,
-        },
-      })
-    }
+  const handleClickTag = (tag: string) => {
+    router.push({
+      query: {
+        ...router.query,
+        tag: tag === currentTag ? "" : tag,
+      },
+    })
   }
 
   return (
     <StyledWrapper>
-      <div className="top">
-        <Emoji>🏷️</Emoji> Tags
-      </div>
-      <div className="list">
+      <div className="tag-container">
         {Object.keys(data).map((key) => (
           <a
             key={key}
+            className="tag-item"
             data-active={key === currentTag}
             onClick={() => handleClickTag(key)}
           >
@@ -55,58 +41,25 @@ const TagList: React.FC<Props> = () => {
 export default TagList
 
 const StyledWrapper = styled.div`
-  .top {
-    display: none;
-    padding: 0.25rem;
-    margin-bottom: 0.75rem;
-
-    @media (min-width: 1024px) {
-      display: block;
-    }
+  .tag-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-end;
+    gap: 0.5rem;
+    margin-top: 1rem;
+    padding: 1rem;
   }
 
-  .list {
-    display: flex;
-    margin-bottom: 1.5rem;
-    gap: 0.25rem;
-    overflow: scroll;
-
-    scrollbar-width: none;
-    -ms-overflow-style: none;
-    ::-webkit-scrollbar {
-      width: 0;
-      height: 0;
-    }
-
-    @media (min-width: 1024px) {
-      display: block;
-    }
-
-    a {
-      display: block;
-      padding: 0.25rem;
-      padding-left: 1rem;
-      padding-right: 1rem;
-      margin-top: 0.25rem;
-      margin-bottom: 0.25rem;
-      border-radius: 0.75rem;
-      font-size: 0.875rem;
-      line-height: 1.25rem;
-      color: ${({ theme }) => theme.colors.gray10};
-      flex-shrink: 0;
-      cursor: pointer;
-
-      :hover {
-        background-color: ${({ theme }) => theme.colors.gray4};
-      }
-      &[data-active="true"] {
-        color: ${({ theme }) => theme.colors.gray12};
-        background-color: ${({ theme }) => theme.colors.gray4};
-
-        :hover {
-          background-color: ${({ theme }) => theme.colors.gray4};
-        }
-      }
+  .tag-item {
+    font-size: 0.9rem;
+    padding: 0.2rem 0.5rem;
+    border-radius: 0.25rem;
+    background-color: ${(props) => props.theme.colors.gray};
+    cursor: pointer;
+    
+    &[data-active="true"] {
+      background-color: ${(props) => props.theme.colors.primary};
+      color: white;
     }
   }
 `
